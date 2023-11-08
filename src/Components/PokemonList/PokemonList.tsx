ae0dfa8d4acf839101b3cc1b7c3d4cb1cddf251b
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-11-08 17:22:48 -0500

    message

--- src/Components/PokemonList/PokemonList.tsx
+++ src/Components/PokemonList/PokemonList.tsx
@@ -14,23 +14,37 @@ const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
     setPokemonData(selectedPokemon);
   };
 
+  const handleResetClick = () => {
+    // Reset the selected Pokemon and set pokemonData to null
+    setPokemonData(null);
+  };
+
   return (
     <div className="PokemonList">
-      <h2>Pokémon List</h2>
-      <ul>
-        {pokemons.map((pokemon) => (
-          <li key={pokemon.id} onClick={() => handlePokemonClick(pokemon)}>
-            {pokemon.name}
-          </li>
-        ))}
-      </ul>
-      {pokemonData && (
+      {pokemonData ? (
+        // Display selected Pokemon details if a Pokemon is selected
         <div>
           <h2>Selected Pokemon</h2>
           <p>ID: {pokemonData.id}</p>
           <p>Name: {pokemonData.name}</p>
-          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`} alt={pokemonData.name} />
-
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`}
+            alt={pokemonData.name}
+          />
+          <button onClick={handleResetClick}>Reset</button>
+        </div>
+      ) : (
+        // Display list of Pokemon if no Pokemon is selected
+        <div>
+          <h2>Pokémon List</h2>
+          
+          <ul>
+            {pokemons.map((pokemon) => (
+              <li key={pokemon.id} onClick={() => handlePokemonClick(pokemon)}>
+                {pokemon.name}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
@@ -38,3 +52,4 @@ const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
 };
 
 export default PokemonList;
+
