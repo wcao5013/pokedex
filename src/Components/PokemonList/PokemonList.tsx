9254d5ec3fc8b56138314a4e76814d33c6d8a510
Wan Cao <36395075+wcao5013@users.noreply.github.com>
2023-11-09 15:37:13 -0500

    message

--- src/Components/PokemonList/PokemonList.tsx
+++ src/Components/PokemonList/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { usePokemonContext } from '../../Context/PokemonContext'; // Update the path accordingly
 import { Pokemon } from '../../Pokemons/pokemon';
 
@@ -9,23 +9,32 @@ interface PokemonListProps {
 }
 
 const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
-  const { pokemonData, setPokemonData } = usePokemonContext(); // Access context values
+  const { pokemonData, setPokemonData } = usePokemonContext();
+  const { fetchPokemonData } = usePokemonContext() // Access context values
 
+ 
+  
   const handlePokemonClick = (selectedPokemon: Pokemon) => {
     // When a Pokemon is clicked, update the context data
-    setPokemonData(selectedPokemon);
+    const searchTerm = selectedPokemon.name; // Set the search term dynamically or based on user input
+    fetchPokemonData(searchTerm);
   };
 
   const handleResetClick = () => {
     // Reset the selected Pokemon and set pokemonData to null
     setPokemonData(null);
+    console.log('This is just handleResekClick', pokemonData)
   };
+
+ console.log("Updated Pokemon Data", pokemonData)
+
+ 
 
   return (
     <div className="PokemonList">
       {pokemonData ? (
         // Display selected Pokemon details if a Pokemon is selected
-        <div>
+        <div >
           <h2>Selected Pokemon</h2>
           <p>ID: {pokemonData.id}</p>
           <p>Name: {pokemonData.name}</p>
@@ -35,7 +44,7 @@ const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
             alt={pokemonData.name}
           />
           {pokemonData.types && (
-            <p key={pokemonData.id}>Types: {pokemonData.types.map((type) => type.type.name).join(', ')}</p>
+            <p>Types: {pokemonData.types.map((type) => type.type.name).join(', ')}</p>
           )}
           <p>Weight: {pokemonData.weight} kg</p>
           <button onClick={handleResetClick}>Reset</button>
@@ -57,6 +66,7 @@ const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
                 />
                 {pokemon.name}
               </li>
+
             ))}
           </ul>
         </div>
--- src/Context/PokemonContext.tsx
+++ src/Context/PokemonContext.tsx
@@ -1,28 +1,54 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { Pokemon } from '../Pokemons/pokemon';
 
+
 interface PokemonContextProps {
   children: ReactNode;
 }
 
-interface PokemonContextType {
+interface PokemonContextValue {
   pokemonData: Pokemon | null;
   setPokemonData: React.Dispatch<React.SetStateAction<Pokemon | null>>;
+  fetchPokemonData: (searchTerm: string) => Promise<void>;
 }
 
-const PokemonContext = createContext<PokemonContextType | undefined>(undefined);
+const PokemonContext = createContext<PokemonContextValue | undefined>(undefined);
 
 export const PokemonProvider: React.FC<PokemonContextProps> = ({ children }) => {
   const [pokemonData, setPokemonData] = useState<Pokemon | null>(null);
 
+  const fetchPokemonData = async (searchTerm: string) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`);
+      const data = await response.json();
+      setPokemonData({
+        id: data.id,
+        name: data.name,
+        types: data.types,
+        weight: data.weight,
+        height: data.height,
+        sprites: {
+          other: {
+            "official-artwork": {
+              front_default: data.sprites.other['official-artwork'].front_default,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setPokemonData(null);
+    }
+  };
+
   return (
-    <PokemonContext.Provider value={{ pokemonData, setPokemonData }}>
+    <PokemonContext.Provider value={{ pokemonData, setPokemonData, fetchPokemonData }}>
       {children}
     </PokemonContext.Provider>
   );
 };
 
-export const usePokemonContext = (): PokemonContextType => {
+export const usePokemonContext = (): PokemonContextValue => {
   const context = useContext(PokemonContext);
   if (!context) {
     throw new Error('usePokemonContext must be used within a PokemonProvider');
